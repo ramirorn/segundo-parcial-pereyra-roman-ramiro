aadb6a8f06a160c641003aae54839d723345a4e8
Ramiro <ramiroroman306@gmail.com>
2025-06-27 10:50:01 -0300

    Se crearon las rutas para las peliculas con sus métodos correspondientes

---
+++ src/routes/movie.routes.js
@@ -0,0 +1,20 @@
+// Importacion del enrutador de express
+import { Router } from "express";
+
+// Importacion de los controladores para las peliculas
+import { 
+    deleteMovie,
+    createMovie,
+    updateMovie,
+    getAllMovies,
+    getMovieByID
+} from "../controllers/movie.controllers";
+
+
+export const moviesRoutes = Router();
+
+moviesRoutes.get("/movies", getAllMovies);
+moviesRoutes.get("/movies/:id", getMovieByID);
+moviesRoutes.post("/movies", createMovie);
+moviesRoutes.put("/movies/:id", updateMovie);
+moviesRoutes.delete("/movies/:id", deleteMovie);
