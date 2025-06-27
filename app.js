ba3d96e830c86c82afdb9c141925f2b2f0883db0
Ramiro <ramiroroman306@gmail.com>
2025-06-27 10:58:48 -0300

    Se inicializo el servidor en app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import { startDB } from "./src/config/database.js";
+import { moviesRoutes } from "./src/routes/movie.routes.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+
+app.use(express.json());
+app.use("/api", moviesRoutes);
+
+app.listen(PORT, async () => {
+    await startDB;
+    console.log(`SERVIDOR CORRIENDO EN: http://localhost:${PORT}/api`)
+})
